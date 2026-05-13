803122b3cb5f0c35365676f41b2b140c49b3697c
gbrl08snts <gabriel.aquino.069@ufrn.edu.br>
2026-05-13 16:31:49 -0300

    Implementando SJF

--- schedulers/sjf.c
+++ schedulers/sjf.c
@@ -20,6 +20,47 @@ struct proc * scheduler(struct proc * current)
 {
     struct proc * selected; 
 
-    return NULL;
-}
+    if (current != NULL)
+    {
+        switch (current->state) 
+        {
+            case READY:
+                enqueue(ready, current);
+                break;
 
+            case BLOCKED:
+                enqueue(blocked, current);
+                break;
+
+            case FINISHED:
+                enqueue(finished, current);
+                break;
+
+            default:
+                printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
+        }
+    }
+
+    if (isempty(ready))
+        return NULL;
+
+    // Pega o primeiro da fila.
+    struct proc * shortest = ready->head;
+    struct proc * aux = shortest->next;
+
+    // Procura o processo com menor remaining_time.
+    while (aux != NULL)
+    {
+        if (aux->remaining_time < shortest->remaining_time)
+            shortest = aux;
+
+        aux = aux->next;
+    }
+
+    // Remove da fila o processo escolhido.
+    selected = dequeue_bypid(ready, shortest->pid);
+
+    selected->state = RUNNING;
+
+    return selected;
+}
